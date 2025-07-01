fbfcf334bcd402eab8242ccdd2880a687d7c4038
Avi Parampampam <aviparampampam@gmail.com>
2025-07-01 20:16:14 +0500

    fix(logging): specified LevelDebug for default logger

--- logging/logger.go
+++ logging/logger.go
@@ -2,13 +2,24 @@ package logging
 
 import (
 	"os"
+	"time"
 
 	"github.com/av1ppp/logx"
 	"github.com/av1ppp/logx/handlercolor1"
+	"github.com/fatih/color"
 )
 
 func New() *logx.Logger {
-	return logx.New(handlercolor1.New(os.Stdout, handlercolor1.DefaultOptions)).With(
+	return logx.New(handlercolor1.New(os.Stdout, &handlercolor1.Options{
+		Level:         logx.LevelDebug,
+		TimeFormat:    time.DateTime,
+		SrcFileMode:   handlercolor1.ShortFile,
+		SrcFileLength: 0,
+		MsgPrefix:     color.HiWhiteString("| "),
+		MsgLength:     0,
+		MsgColor:      color.New(),
+		NoColor:       false,
+	})).With(
 		logx.Module("onvif"),
 	)
 }
